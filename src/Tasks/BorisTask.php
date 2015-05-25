06fe090d0609eb22820802ebe602b6761d251bc5
Michael Karbowiak <michael@karbowiak.dk>
2015-05-25 03:21:15 +0200

    remove resquecron and replace it with resquetask, add boris and add the task to start it

--- src/Tasks/BorisTask.php
+++ src/Tasks/BorisTask.php
@@ -3,42 +3,34 @@
  * Created by PhpStorm.
  * User: micha
  * Date: 25-05-2015
- * Time: 02:35
+ * Time: 03:04
  */
 
 namespace ProjectRena\Tasks;
 
-
+use ProjectRena;
+use ProjectRena\Controller;
+use ProjectRena\Lib;
+use ProjectRena\Model;
+use ProjectRena\Tasks;
+use ProjectRena\Tasks\Cronjobs;
+use ProjectRena\Tasks\Resque;
 use Cilex\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-
-/**
- * Class ResqueCron
- * @package ProjectRena\Tasks
- */
-class ResqueCron extends Command {
-    /**
-     *
-     */
+class BorisTask extends Command {
     protected function configure()
     {
         $this
-            ->setName("resque:run")
-            ->setDescription("Fires up resque");
+            ->setName("boris")
+            ->setDescription("Starts boris");
     }
 
-    /**
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        putenv("VERBOSE=0");
-        putenv("QUEUE=*");
-
-        include(__DIR__ . "/../../vendor/chrisboulton/php-resque/resque.php");
+        $boris = new \Boris\Boris("Rena> ");
+        $boris->start();
     }
 }
---
+++ src/Tasks/ResqueTask.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: micha
+ * Date: 25-05-2015
+ * Time: 02:35
+ */
+
+namespace ProjectRena\Tasks;
+
+
+use Cilex\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class ResqueCron
+ * @package ProjectRena\Tasks
+ */
+class ResqueTask extends Command {
+    /**
+     *
+     */
+    protected function configure()
+    {
+        $this
+            ->setName("resque:run")
+            ->setDescription("Fires up resque");
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        putenv("VERBOSE=0");
+        putenv("QUEUE=*");
+
+        include(__DIR__ . "/../../vendor/chrisboulton/php-resque/resque.php");
+    }
+}
