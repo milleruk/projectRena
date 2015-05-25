6054e615562e5dc285dba46ffb7baf8c17209ffa
Michael Karbowiak <michael@karbowiak.dk>
2015-05-25 02:00:18 +0200

    Add users and configuration table migrations

---
+++ migrations/20150524235428_configuration.php
@@ -0,0 +1,39 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Configuration extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     *
+    public function change()
+    {
+    }
+    */
+    
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $users = $this->table("configuration");
+        $users
+            ->addColumn("key", "string", array("limit" => 250))
+            ->addColumn("value", "string", array("limit" => 250))
+            ->save();
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+
+    }
+}
