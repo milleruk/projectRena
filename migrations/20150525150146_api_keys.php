08187e78ccd1a06fc3578ca27b4ad417692e75dd
Michael Karbowiak <michael@karbowiak.dk>
2015-05-25 19:21:16 +0200

    Add apiKeys table

--- migrations/20150525150146_api_keys.php
+++ migrations/20150525150146_api_keys.php
@@ -22,7 +22,18 @@ class ApiKeys extends AbstractMigration
      */
     public function up()
     {
-    
+		$apiKeys = $this->table("apiKeys");
+		$apiKeys
+			->addColumn("keyID", "string", array("limit" => 250))
+			->addColumn("vCode", "string", array("limit" => 250))
+			->addColumn("userID", "int", array("limit" => 11))
+			->addColumn("errorCode", "int", array("limit" => 4))
+			->addColumn("dateAdded", "datetime", array("default" => "CURRENT_TIMESTAMP"))
+			->addColumn("lastValidation", "datetime", array("default" => "0000-00-00 00:00:00"))
+			->addIndex(array("keyID"))
+			->addIndex(array("keyID", "vCode"))
+			->addIndex(array("keyID", "dateAdded"))
+			->save();
     }
 
     /**
