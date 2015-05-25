6e149d5c68e4a35955d1afbfc357905de62dd310
Michael Karbowiak <michael@karbowiak.dk>
2015-05-25 21:11:09 +0200

    Tell  it's a Slim

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,12 +5,12 @@ namespace ProjectRena\Controller;
 
 
 class IndexController {
-	public static function hello($app)
+	public static function hello(\Slim\Slim $app)
 	{
 		$app->render("index.html");
 	}
 
-	public static function helloName($app, $name)
+	public static function helloName(\Slim\Slim $app, $name)
 	{
 		$app->render("index.html", array("name" => $name));
 	}
