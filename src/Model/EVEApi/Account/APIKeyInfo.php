b843da1f824cd6649e6a5094b1a4349cd2dec22e
Michael Karbowiak <michael@karbowiak.dk>
2015-05-28 22:02:47 +0200

    Forgot phpdoc for APIKeyInfo and Characters

--- src/Model/EVEApi/Account/APIKeyInfo.php
+++ src/Model/EVEApi/Account/APIKeyInfo.php
@@ -3,9 +3,28 @@
 
 namespace ProjectRena\Model\EVEApi\Account;
 
-
+/**
+ * Class APIKeyInfo
+ *
+ * @package ProjectRena\Model\EVEApi\Account
+ */
+/**
+ * Class APIKeyInfo
+ *
+ * @package ProjectRena\Model\EVEApi\Account
+ */
 class APIKeyInfo {
+	/**
+	 * @var null
+	 */
 	public static $accessMask = null;
+
+	/**
+	 * @param $apiKey
+	 * @param $vCode
+	 *
+	 * @return mixed
+	 */
 	public static function getData($apiKey, $vCode)
 	{
 		$pheal = PhealLoader::loadPheal($apiKey, $vCode);
--- src/Model/EVEApi/Account/Characters.php
+++ src/Model/EVEApi/Account/Characters.php
@@ -3,9 +3,28 @@
 
 namespace ProjectRena\Model\EVEApi\Account;
 
-
+/**
+ * Class Characters
+ *
+ * @package ProjectRena\Model\EVEApi\Account
+ */
+/**
+ * Class Characters
+ *
+ * @package ProjectRena\Model\EVEApi\Account
+ */
 class Characters {
+	/**
+	 * @var null
+	 */
 	public static $accessMask = null;
+
+	/**
+	 * @param $apiKey
+	 * @param $vCode
+	 *
+	 * @return mixed
+	 */
 	public static function getData($apiKey, $vCode)
 	{
 		$pheal = PhealLoader::loadPheal($apiKey, $vCode);
