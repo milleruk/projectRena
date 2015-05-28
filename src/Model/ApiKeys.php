d167c837108d44b6751aa50f537df09cfb64c2e6
Michael Karbowiak <michael@karbowiak.dk>
2015-05-28 23:23:08 +0200

    not

--- src/Model/ApiKeys.php
+++ src/Model/ApiKeys.php
@@ -19,40 +19,40 @@ class ApiKeys {
 	 *
 	 * @return bool|int|string
 	 */
-	public static function addAPIKey($apiKeyID, $vCode, $userID = NULL)
+	public static function addAPIKey($apiKey, $vCode, $userID = NULL)
 	{
-		return Database::execute("INSERT INTO apiKeys (keyID, vCode, userID) VALUES (:keyID, :vCode, :userID)", array(":keyID" => $apiKeyID, ":vCode" => $vCode, ":userID" => $userID));
+		return Database::execute("INSERT INTO apiKeys (keyID, vCode, userID) VALUES (:keyID, :vCode, :userID)", array(":keyID" => $apiKey, ":vCode" => $vCode, ":userID" => $userID));
 	}
 
 	/**
-	 * @param $apiKeyID
+	 * @param $apiKey
 	 *
 	 * @return array
 	 */
-	public static function getAPIKey($apiKeyID)
+	public static function getAPIKey($apiKey)
 	{
-		return Database::queryRow("SELECT * FROM apiKeys WHERE keyID = :keyID", array(":keyID" => $apiKeyID));
+		return Database::queryRow("SELECT * FROM apiKeys WHERE keyID = :keyID", array(":keyID" => $apiKey));
 	}
 
 	/**
-	 * @param $apiKeyID
+	 * @param $apiKey
 	 * @param $vCode
 	 * @param $userID
 	 *
 	 * @return bool|int|string
 	 */
-	public static function updateAPIKey($apiKeyID, $vCode, $userID)
+	public static function updateAPIKey($apiKey, $vCode, $userID)
 	{
-		return Database::execute("INSERT INTO apiKeys (keyID, vCode, userID) VALUES (:keyID, :vCode, :userID) ON DUPLICATE UPDATE keyID = :keyID, vCode = :vCode, userID = :userID", array(":keyID" => $apiKeyID, ":vCode" => $vCode, ":userID" => $userID));
+		return Database::execute("INSERT INTO apiKeys (keyID, vCode, userID) VALUES (:keyID, :vCode, :userID) ON DUPLICATE UPDATE keyID = :keyID, vCode = :vCode, userID = :userID", array(":keyID" => $apiKey, ":vCode" => $vCode, ":userID" => $userID));
 	}
 
 	/**
-	 * @param $apiKeyID
+	 * @param $apiKey
 	 *
 	 * @return bool|int|string
 	 */
-	public static function deleteAPIKey($apiKeyID)
+	public static function deleteAPIKey($apiKey)
 	{
-		return Database::execute("DELETE FROM apiKeys WHERE keyID = :keyID", array(":keyID" => $apiKeyID));
+		return Database::execute("DELETE FROM apiKeys WHERE keyID = :keyID", array(":keyID" => $apiKey));
 	}
 }
