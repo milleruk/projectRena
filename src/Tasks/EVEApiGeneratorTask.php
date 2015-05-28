f06dc805ba76d2f95488418605ff58682f1ee186
Michael Karbowiak <michael@karbowiak.dk>
2015-05-29 00:18:02 +0200

    Add the EVEApi.php generator task, yes it's ugly.. deal with it

--- src/Tasks/EVEApiGeneratorTask.php
+++ src/Tasks/EVEApiGeneratorTask.php
@@ -21,19 +21,71 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class BorisTask extends Command {
+class EVEApiGeneratorTask extends Command {
     protected function configure()
     {
         $this
-            ->setName("boris")
-            ->setDescription("Starts boris");
+            ->setName("EVEApiGenerator")
+            ->setDescription("Generates the EVEApi.php model");
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+		// Beginning of the model
+		$model = "<?php
 
-        $boris = new \Boris\Boris("Rena> ");
-        $boris->setLocal(array("Database" => new Lib\Database()));
-        $boris->start();
+namespace ProjectRena\Model\EVEApi;
+
+/**
+ * Class EVEApi
+ *
+ * @package ProjectRena\Model\EVEApi
+ */
+class EVEApi {
+";
+
+		// Load all the files and create calls
+		$dirPath = __DIR__ . "/../Model/EVEApi/*/*.php";
+		$phpFiles = glob($dirPath);
+
+		foreach($phpFiles as $file)
+		{
+			$elements = explode("/", $file);
+			$className = str_replace(".php", "", $elements[10]);
+			$modelPath = "\ProjectRena\\$elements[7]\\$elements[8]\\$elements[9]\\$className";
+
+			$type = "";
+			if($elements[9] == "Character")
+				$type = "char";
+			elseif($elements[9] == "Corporation")
+				$type = "corp";
+
+			$fileData = file($file);
+
+			foreach($fileData as $key => $line)
+				if(stristr($line, "public function getData("))
+					$functionLine = trim($line);
+
+			$vars = explode("(", $functionLine);
+			$vars = str_replace(")", "", $vars[1]);
+			$vars = str_replace("array", "array()", $vars);
+			$functionLine = str_replace("getData", $className, $functionLine);
+			$functionLine = str_replace("public ", "public static ", $functionLine);
+
+			if(!empty($type))
+				$functionLine = str_replace($className, $type . ucfirst($className), $functionLine);
+
+			$model .= '	' . $functionLine . ' {
+		$return = new ' . $modelPath . '();
+		return $return->getData(' . $vars . ');
+	}
+';
+		}
+
+		// End of the model
+		$model .= "
+};";
+		$modelPath = __DIR__ . "/../Model/EVEApi.php";
+		file_put_contents($modelPath, $model);
     }
 }
