24ef1a022f57ebc480475d71a1459c7c9c6c68b2
Michael Karbowiak <michael@karbowiak.dk>
2015-05-30 22:56:24 +0200

    renaconfig

--- config/10_rena_config.php
+++ config/10_rena_config.php
@@ -1,10 +1,4 @@
 <?php
-// we return a singleton instance of renas config here, this allows us to
-// access this abomination from within everywhere where we have access to the container
-
-$app->container->singleton(
-    'renaConfig',
-    function ($container) {
-        return new \ProjectRena\Model\Config();
-    }
-);
+$app->container->singleton('renaConfig', function ($container) {
+    return new \ProjectRena\Model\Config();
+});
