205ddb7bceb1d5727d6a0e745d536e07e6162c5f
Michael Karbowiak <michael@karbowiak.dk>
2015-05-30 23:02:47 +0200

    Undo app.php and index.php changes

--- public/index.php
+++ public/index.php
@@ -1,23 +1,3 @@
 <?php
-// Shit here is whining about stuff not being loaded, but it is under app.php.. ignore it..
 
-include '../app.php';
-
-// Session
-$session = new SessionHandler();
-session_set_save_handler($session, true);
-session_cache_limiter(false);
-session_start();
-
-// Launch Whoops
-/** @noinspection PhpParamsInspection */
-/** @noinspection PhpUndefinedClassInspection */
-$app->add(new WhoopsMiddleware());
-
-// Prepare view
-/** @noinspection PhpUndefinedClassInspection */
-$app->view(new Twig());
-$app->view->parserOptions = $config['twig'];
-
-// Run app
-$app->run();
+require_once '../app.php';
