a2d5cef85c36ab489a7111bd3652991b6401f8ac
Michael Karbowiak <michael@karbowiak.dk>
2015-05-31 20:54:38 +0200

    Add paste table

--- migrations/20150531184410_paste.php
+++ migrations/20150531184410_paste.php
@@ -27,6 +27,7 @@ class Paste extends AbstractMigration
             ->addColumn('hash', 'string', array('limit' => 64))
             ->addColumn('userID', 'integer', array('limit' => 5))
             ->addColumn('data', 'text', array("limit" => MysqlAdapter::TEXT_LONG))
+            ->addColumn('timeout', 'datetime', array("default" => '0000-00-00 00:00:00'))
             ->addColumn('created', 'datetime', array("default" => 'CURRENT_TIMESTAMP'))
             ->addIndex(array('hash'), array('unique' => true))
             ->save();
