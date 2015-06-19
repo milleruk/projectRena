bb749cd7550b1203f384fc842417274c1071a426
Michael Karbowiak <michael@karbowiak.dk>
2015-06-19 21:17:05 +0200

    add dgmTypeEffects

---
+++ src/Model/EVE/dgmTypeEffects.php
@@ -0,0 +1,60 @@
+<?php
+namespace ProjectRena\Model\EVE;
+
+use ProjectRena\RenaApp;
+
+/**
+ * Class invTypes
+ *
+ * @package ProjectRena\Model\EVE
+ */
+class dgmTypeEffects
+{
+	/**
+	 * @var RenaApp
+	 */
+	private $app;
+	/**
+	 * @var \ProjectRena\Lib\Db
+	 */
+	private $db;
+
+	/**
+	 * @param RenaApp $app
+	 */
+	function __construct(RenaApp $app)
+	{
+		$this->app = $app;
+		$this->db = $this->app->Db;
+	}
+
+	/**
+	 * @param $typeID
+	 *
+	 * @return array
+	 */
+	function getAllByID($typeID)
+	{
+		return $this->db->queryRow("SELECT * FROM dgmTypeEffects WHERE typeID = :id", array(":id" => $typeID), 3600);
+	}
+
+	/**
+	 * @param $typeID
+	 *
+	 * @return null
+	 */
+	function getEffectIDByID($typeID)
+	{
+		return $this->db->queryField("SELECT effectID FROM dgmTypeEffects WHERE typeID = :id", "effectID", array(":id" => $typeID), 3600);
+	}
+
+	/**
+	 * @param $typeID
+	 *
+	 * @return null
+	 */
+	function getIsDefaultByID($typeID)
+	{
+		return $this->db->queryField("SELECT isDefault FROM dgmTypeEffects WHERE typeID = :id", "isDefault", array(":id" => $typeID), 3600);
+	}
+}
