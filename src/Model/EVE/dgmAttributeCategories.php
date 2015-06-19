2d836cc26cb1a230d1e892621667688d3585261a
Michael Karbowiak <michael@karbowiak.dk>
2015-06-19 20:33:57 +0200

    Add dgmAttributeCategories

---
+++ src/Model/EVE/dgmAttributeCategories.php
@@ -0,0 +1,71 @@
+<?php
+namespace ProjectRena\Model\EVE;
+
+use ProjectRena\RenaApp;
+
+/**
+ * Class invTypes
+ *
+ * @package ProjectRena\Model\EVE
+ */
+class dgmAttributeCategories
+{
+	/**
+	 * @var RenaApp
+	 */
+	private $app;
+	/**
+	 * @var \ProjectRena\Lib\Db
+	 */
+	private $db;
+
+	/**
+	 * @param RenaApp $app
+	 */
+	function __construct(RenaApp $app)
+	{
+		$this->app = $app;
+		$this->db = $this->app->Db;
+	}
+
+	/**
+	 * @param $categoryID
+	 *
+	 * @return array
+	 */
+	public function getAllByID($categoryID)
+	{
+		return $this->db->queryRow("SELECT * FROM dgmAttributeCategories WHERE categoryID = :id", array(":id" => $categoryID));
+	}
+
+	/**
+	 * @param $categoryName
+	 *
+	 * @return array
+	 */
+	public function getAllByName($categoryName)
+	{
+		return $this->db->queryRow("SELECT * FROM dgmAttributeCategories WHERE categoryName = :name", array(":name" => $categoryName));
+	}
+
+	/**
+	 * @param $categoryID
+	 *
+	 * @return null
+	 */
+	public function getCategoryDescriptionByID($categoryID)
+	{
+		return $this->db->queryField("SELECT categoryDescription FROM dgmAttributeCategories WHERE categoryID = :id", "categoryDescription", arraY(":id" => $categoryID));
+	}
+
+	/**
+	 * @param $categoryName
+	 *
+	 * @return null
+	 */
+	public function getCategoryDescriptionByName($categoryName)
+	{
+		return $this->db->queryField("SELECT categoryDescription FROM dgmAttributeCategories WHERE categoryName = :name", "categoryDescription", arraY(":name" => $categoryName));
+	}
+
+}
