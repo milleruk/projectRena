59a7ef2173c17dbcbb6498f0b7b0d4d6799dba4b
Michael Karbowiak <michael@karbowiak.dk>
2015-06-19 21:24:53 +0200

    add invGroups

---
+++ src/Model/EVE/invGroups.php
@@ -0,0 +1,50 @@
+<?php
+namespace ProjectRena\Model\EVE;
+
+use ProjectRena\RenaApp;
+
+/**
+ * Class invTypes
+ *
+ * @package ProjectRena\Model\EVE
+ */
+class invGroups
+{
+	/**
+	 * @var RenaApp
+	 */
+	private $app;
+	/**
+	 * @var \ProjectRena\Lib\Db
+	 */
+	private $db;
+
+	/**
+	 * @param RenaApp $app
+	 */
+	function __construct(RenaApp $app)
+	{
+		$this->app = $app;
+		$this->db = $this->app->Db;
+	}
+
+	/**
+	 * @param $groupID
+	 *
+	 * @return array
+	 */
+	public function getAllByID($groupID)
+	{
+		return $this->db->queryRow("SELECT * FROM invGroups WHERE groupID = :id", array(":id" => $groupID), 3600);
+	}
+
+	/**
+	 * @param $groupName
+	 *
+	 * @return array
+	 */
+	public function getAllByName($groupName)
+	{
+		return $this->db->queryRow("SELECT * FROM invGroups WHERE groupName = :name", array(":name" => $groupName), 3600);
+	}
+}
