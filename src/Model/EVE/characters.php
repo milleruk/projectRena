46819b52fbecc4b5f1ec8c3b5276361249241c97
Michael Karbowiak <michael@karbowiak.dk>
2015-06-20 22:15:36 +0200

    Add/Update characters.php

--- src/Model/EVE/characters.php
+++ src/Model/EVE/characters.php
@@ -28,5 +28,51 @@ class characters
         $this->db = $this->app->Db;
     }
 
+    /**
+     * @param $characterID
+     */
+    public function getAllByID($characterID)
+    {
+        $this->db->queryRow("SELECT * FROM characters WHERE characterID = :id", array(":id" => $characterID), 3600);
+    }
 
+    /**
+     * @param $characterName
+     */
+    public function getAllByName($characterName)
+    {
+        $this->db->queryRow("SELECT * FROM characters WHERE characterName = :name", array(":name" => $characterName), 3600);
+    }
+
+    /**
+     * @param $corporationID
+     */
+    public function getAllByCorporationID($corporationID)
+    {
+        $this->db->queryRow("SELECT * FROM characters WHERE corporationID = :id", array(":id" => $corporationID), 3600);
+    }
+
+    /**
+     * @param $allianceID
+     */
+    public function getAllByAllianceID($allianceID)
+    {
+        $this->db->queryRow("SELECT * FROM characters WHERE allianceID = :id", array(":id" => $allianceID), 3600);
+    }
+
+    /**
+     * @param $characterID
+     */
+    public function getHistoryByID($characterID)
+    {
+        $this->db->queryField("SELECT history FROM characters WHERE characterID = :id", "history", array(":id" => $characterID), 3600);
+    }
+
+    /**
+     * @param $characterName
+     */
+    public function getHistoryByName($characterName)
+    {
+        $this->db->queryField("SELECT history FROM characters WHERE characterName = :name", "history", array(":name" => $characterName), 3600);
+    }
 }
