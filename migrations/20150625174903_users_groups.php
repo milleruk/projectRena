dbdac3dd112afca81a5ba95bcb599ddadbe4343d
Michael Karbowiak <michael@karbowiak.dk>
2015-06-25 20:02:55 +0200

    Add usersGroups and groups migrations

---
+++ migrations/20150625174903_users_groups.php
@@ -0,0 +1,28 @@
+<?php
+
+use Phinx\Db\Adapter\MysqlAdapter;
+use Phinx\Migration\AbstractMigration;
+
+class UsersGroups extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     */
+    public function change()
+    {
+        $usersGroups = $this->table("usersGroups", array("engine" => "TokuDB"));
+        $usersGroups
+            ->addColumn("userID", "integer", array("limit" => 11))
+            ->addColumn("groupID", "integer", array("limit" => 11))
+            ->addColumn("created", "datetime", array("default" => "CURRENT_TIMESTAMP"))
+            ->addIndex(array("userID", "groupID"), array("unique" => true))
+            ->addIndex(array("userID"))
+            ->addIndex(array("groupID"))
+            ->save();
+    }
+}
---
+++ migrations/20150625180042_groups.php
@@ -0,0 +1,32 @@
+<?php
+
+use Phinx\Db\Adapter\MysqlAdapter;
+use Phinx\Migration\AbstractMigration;
+
+class Groups extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     */
+    public function change()
+    {
+        $groups = $this->table("groups", array("engine" => "TokuDB"));
+        $groups
+            ->addColumn("groupID", "integer", array("limit" => 11))
+            ->addColumn("groupName", "string", array("limit" => 255))
+            ->addColumn("ownerID", "integer", array("limit" => 11))
+            ->addColumn("admins", "text", array("limit" => MysqlAdapter::TEXT_MEDIUM)) // Json array with userIDs that can admin the group (Change settings etc)
+            ->addColumn("moderators", "text", array("limit" => MysqlAdapter::TEXT_MEDIUM)) // Json array with userIDs that can moderate the group (Add/Kick users)
+            ->addColumn("created", "datetime", array("default" => "CURRENT_TIMESTAMP"))
+            ->addIndex(array("groupName"), array("unique" => true))
+            ->addIndex(array("groupID"))
+            ->addIndex(array("ownerID"))
+            ->save();
+
+    }
+}
