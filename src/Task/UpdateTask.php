afd4199978a47b071434006624624160ad479250
Michael Karbowiak <michael@karbowiak.dk>
2015-06-26 00:02:12 +0200

    Fix Update so you need to do -cu to run composer and unittest

--- src/Task/UpdateTask.php
+++ src/Task/UpdateTask.php
@@ -5,6 +5,7 @@ use Cilex\Command\Command;
 use ProjectRena\Lib;
 use ProjectRena\RenaApp;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
