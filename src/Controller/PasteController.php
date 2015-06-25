438c5e308cf9336cf4c374030efca8946c7387ba
Michael Karbowiak <michael@karbowiak.dk>
2015-06-25 19:47:07 +0200

    Update paste controller

--- src/Controller/PasteController.php
+++ src/Controller/PasteController.php
@@ -3,42 +3,63 @@ namespace ProjectRena\Controller;
 
 use ProjectRena\RenaApp;
 
+/**
+ * Class PasteController
+ *
+ * @package ProjectRena\Controller
+ */
 class PasteController
 {
-				protected $app;
+    /**
+     * @var RenaApp
+     */
+    protected $app;
 
-				function __construct(RenaApp $app)
-				{
-								$this->app = $app;
-				}
+    /**
+     * @param RenaApp $app
+     */
+    function __construct(RenaApp $app)
+    {
+        $this->app = $app;
+    }
 
-				public function pastePage()
-				{
-								$this->app->render('/paste/paste.twig');
-				}
+    /**
+     *
+     */
+    public function pastePage()
+    {
+        $this->app->render('/paste/paste.twig');
+    }
 
-				public function postPaste()
-				{
-								$postData = $this->app->request->post("paste");
-								$timeout = $this->app->request->post("timeout");
-								if(isset($_SESSION["characterName"]))
-								{
-												$user = $this->app->users->getUserByName($_SESSION["characterName"]);
-												$userID = $user["id"];
-												$hash = md5(time() . $_SESSION["characterName"] . $_SESSION["characterID"]);
-												$this->app->paste->createPaste($hash, $userID, $postData, $timeout);
-												$this->app->redirect("/paste/{$hash}/");
-								} else
-								{
-												$this->app->render("/paste/paste.twig", array("error" => "Error, you need to be logged in to post data"));
-								}
-				}
+    /**
+     *
+     */
+    public function postPaste()
+    {
+        $postData = $this->app->request->post("paste");
+        $timeout = $this->app->request->post("timeout");
+        if(isset($_SESSION["characterName"]))
+        {
+            $user = $this->app->Users->getUserByName($_SESSION["characterName"]);
+            $userID = $user["id"];
+            $hash = md5(time() . $_SESSION["characterName"] . $_SESSION["characterID"]);
+            $this->app->paste->createPaste($hash, $userID, $postData, $timeout);
+            $this->app->redirect("/paste/{$hash}/");
+        } else
+        {
+            $this->app->render("/paste/paste.twig", array("error" => "Error, you need to be logged in to post data"));
+        }
+    }
 
-				public function showPaste($hash)
-				{
-								$data = $this->app->paste->getPasteData($hash);
-								$pasteData = $data["data"];
-								$pasteCreated = $data["created"];
-								$this->app->render("/paste/pasteShow.twig", array("pasteData" => $pasteData, "pasteCreated" => $pasteCreated));
-				}
+    /**
+     * @param $hash
+     */
+    public function showPaste($hash)
+    {
+        $data = $this->app->Paste->getPasteData($hash);
+        $pasteData = $data["data"];
+        $pasteCreated = $data["created"];
+        $userID = $data["userID"]; // attach user data for the user that created it
+        $this->app->render("/paste/pasteShow.twig", array("pasteData" => $pasteData, "pasteCreated" => $pasteCreated));
+    }
 }
