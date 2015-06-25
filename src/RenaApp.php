99bfa5b659e7427c89414630a61cdaac4156f001
Michael Karbowiak <michael@karbowiak.dk>
2015-06-25 20:28:21 +0200

    Update composer.lock and RenaApp

--- src/RenaApp.php
+++ src/RenaApp.php
@@ -19,6 +19,7 @@ use ProjectRena\Model\Config;
 use ProjectRena\Model\Paste;
 use ProjectRena\Model\Storage;
 use ProjectRena\Model\Users;
+use ProjectRena\Model\UsersGroups;
 use ProjectRena\Model\CCP\dgmAttributeCategories;
 use ProjectRena\Model\CCP\dgmAttributeTypes;
 use ProjectRena\Model\CCP\dgmEffects;
@@ -138,6 +139,7 @@ use ProjectRena\Model\EVEApi\Server\ServerStatus as EVEServerServerStatus;
  * @property Paste Paste
  * @property Storage Storage
  * @property Users Users
+ * @property UsersGroups UsersGroups
  * @property dgmAttributeCategories dgmAttributeCategories
  * @property dgmAttributeTypes dgmAttributeTypes
  * @property dgmEffects dgmEffects
