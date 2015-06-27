a63460f747f9e9ab1c0e3d67a40f769bf85eaa22
Michael Karbowiak <michael@karbowiak.dk>
2015-06-27 19:42:14 +0200

    Add ratchet task, works by sending a request to zmq on port 5555, which then relays the message to every connection on websocket

---
+++ src/Task/RatchetTask.php
@@ -0,0 +1,149 @@
+<?php
+namespace ProjectRena\Task;
+
+use Cilex\Command\Command;
+use Predis\Async\Connection\ConnectionInterface;
+use ProjectRena\Lib;
+use ProjectRena\RenaApp;
+use Ratchet\Wamp\Topic;
+use Ratchet\Wamp\WampServerInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use ZMQ;
+
+/**
+ * Starts up a ratchet server that sends killmails to the user
+ */
+class RatchetTask extends Command
+{
+
+    /**
+     */
+    protected function configure()
+    {
+        $this->setName('ratchet')->setDescription('Starts up a ratchet server');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        //Init rena
+        $app = RenaApp::getInstance();
+
+        // Setup the react event loop and call up the pusher class
+        $loop = \React\EventLoop\Factory::create();
+        $pusher = new Pusher();
+
+        // ZeroMQ server
+        $context = new \React\ZMQ\Context($loop);
+        $pull = $context->getSocket(ZMQ::SOCKET_PULL);
+        $pull->bind("tcp://127.0.0.1:5555");
+        $pull->on("message", array($pusher, "onMessage"));
+
+        // Websocket server
+        $webSock = new \React\Socket\Server($loop);
+        $webSock->listen(8800, "0.0.0.0");
+        $webServer = new \Ratchet\Server\IoServer(new \Ratchet\Http\HttpServer(new \Ratchet\WebSocket\WsServer(new \Ratchet\Wamp\WampServer($pusher))), $webSock);
+        $loop->run();
+    }
+}
+
+/**
+ * Class Pusher
+ *
+ * @package ProjectRena\Task
+ */
+class Pusher implements WampServerInterface
+{
+    /**
+     * @var \SplObjectStorage
+     */
+    protected $clients;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage();
+    }
+
+    /**
+     * @param \Ratchet\ConnectionInterface $conn
+     */
+    function onOpen(\Ratchet\ConnectionInterface $conn)
+    {
+        $this->clients->attach($conn);
+    }
+
+    /**
+     * @param \Ratchet\ConnectionInterface $conn
+     */
+    function onClose(\Ratchet\ConnectionInterface $conn)
+    {
+        $this->clients->detach($conn);
+        $conn->close();
+    }
+
+    /**
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param \Exception $e
+     */
+    function onError(\Ratchet\ConnectionInterface $conn, \Exception $e)
+    {
+        $this->clients->detach($conn);
+        $conn->close();
+    }
+
+    /**
+     * @param $entry
+     */
+    public function onMessage($entry)
+    {
+        //$entryData = json_decode($entry, true);
+        foreach($this->clients as $client)
+            $client->send($entry);
+    }
+
+    /**
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param string $id
+     * @param Topic|string $topic
+     * @param array $params
+     */
+    function onCall(\Ratchet\ConnectionInterface $conn, $id, $topic, array $params)
+    {
+    }
+
+    /**
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param Topic|string $topic
+     */
+    function onSubscribe(\Ratchet\ConnectionInterface $conn, $topic)
+    {
+    }
+
+    /**
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param Topic|string $topic
+     */
+    function onUnSubscribe(\Ratchet\ConnectionInterface $conn, $topic)
+    {
+    }
+
+    /**
+     * @param \Ratchet\ConnectionInterface $conn
+     * @param Topic|string $topic
+     * @param string $event
+     * @param array $exclude
+     * @param array $eligible
+     */
+    function onPublish(\Ratchet\ConnectionInterface $conn, $topic, $event, array $exclude, array $eligible)
+    {
+    }
+}
