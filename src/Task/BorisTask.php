af316b0c6aa34a381d01799459a50dbd0f91e5d2
Michael Karbowiak <michael@karbowiak.dk>
2015-07-09 00:10:10 +0200

    Some minor fuckery with boris, ignore the commented stuff, please..

--- src/Task/BorisTask.php
+++ src/Task/BorisTask.php
@@ -3,6 +3,7 @@
 namespace ProjectRena\Task;
 
 use Cilex\Command\Command;
+use DateTime;
 use ProjectRena\Lib;
 use ProjectRena\RenaApp;
 use Symfony\Component\Console\Input\InputInterface;
@@ -31,11 +32,25 @@ class BorisTask extends Command
 				 */
 				protected function execute(InputInterface $input, OutputInterface $output)
 				{
+								/*
+								$app = \ProjectRena\RenaApp::getInstance();
+								$mails = $app->Db->query("SELECT * FROM killmails WHERE source = 'zkillboardRedisQ'");
+
+								foreach($mails as $mail)
+								{
+												$json = json_decode($mail["kill_json"], true);
+												$killID = $json["killID"];
+												if(!stristr($json["killTime"], "."))
+																continue;
+												$date = DateTime::createFromFormat("Y.m.d H:i:s", $json["killTime"]);
+												$json["killTime"] = $date->format("Y-m-d H:i:s");
+												$json = json_encode($json);
+												$app->Db->execute("UPDATE killmails SET kill_json = :kill_json WHERE killID = :killID", array(":kill_json" => $json, ":killID" => $killID));
+								}*/
 								$boris = new \Boris\Boris('Rena> ');
 								$boris->onStart('$app = \ProjectRena\RenaApp::getInstance();');
 								$boris->onStart('$db = $app->Db;');
 								$boris->onStart('$cache = $app->Cache;');
-								$boris->
 								$boris->start();
 				}
 }
