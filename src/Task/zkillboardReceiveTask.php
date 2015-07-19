5df335e06477dd45a1e0a92e26a14e5a6f73b9c0
Michael Karbowiak <michael@karbowiak.dk>
2015-07-20 00:31:42 +0200

    Remove the echo from the ZKB receiver

--- src/Task/zkillboardReceiveTask.php
+++ src/Task/zkillboardReceiveTask.php
@@ -38,7 +38,6 @@ class zkillboardReceiveTask extends Command
         do
         {
             $p = \RedisQ\Action::listen("redisq.zkillboard.com");
-            var_dump($p);
             if($p["killID"] > $oldKillID)
             {
                 // Get the killmail data.
@@ -59,7 +58,6 @@ class zkillboardReceiveTask extends Command
 
                 // Lets insert the killmail!
                 $app->killmails->insertKillmail($p["killID"], 0, $hash, "zkillboardRedisQ", $json);
-                echo "Inserted a killmail";
             }
             $oldKillID = $p["killID"];
         }
