3a64b73aeabb549d3a127f1ccf3057d3c59f96bd
Michael Karbowiak <karbowiak@gmail.com>
2015-10-26 23:14:21 +0100

    Minor changes

--- src/Model/ApiKeys.php
+++ src/Model/ApiKeys.php
@@ -9,96 +9,96 @@ use ProjectRena\RenaApp;
  */
 class ApiKeys
 {
-				/**
-				 * @var RenaApp
-				 */
-				protected $app;
-				/**
-				 * @var \ProjectRena\Lib\Db
-				 */
-				protected $db;
+    /**
+     * @var RenaApp
+     */
+    protected $app;
+    /**
+     * @var \ProjectRena\Lib\Db
+     */
+    protected $db;
 
-				/**
-				 * @param RenaApp $app
-				 */
-				function __construct(RenaApp $app)
-				{
-								$this->app = $app;
-								$this->db = $app->Db;
-				}
+    /**
+     * @param RenaApp $app
+     */
+    function __construct(RenaApp $app)
+    {
+        $this->app = $app;
+        $this->db = $app->Db;
+    }
 
 
-				/**
-				 * @param $apiKey
-				 * @param $vCode
-				 * @param null $userID
-				 *
-				 * @return boolean
-				 */
+    /**
+     * @param $apiKey
+     * @param $vCode
+     * @param null $userID
+     *
+     * @return boolean
+     */
     public function updateAPIKey($apiKey, $vCode, $userID = null)
-				{
-								return (bool) $this->db->execute('INSERT INTO apiKeys (keyID, vCode, userID) VALUES (:keyID, :vCode, :userID) ON DUPLICATE KEY UPDATE keyID = :keyID, vCode = :vCode, userID = :userID', array(':keyID'  => $apiKey,
-																																																																																																																																																																																																':vCode'  => $vCode,
-																																																																																																																																																																																																':userID' => $userID,
-								));
-				}
+    {
+        return (bool)$this->db->execute('INSERT INTO apiKeys (keyID, vCode, userID) VALUES (:keyID, :vCode, :userID) ON DUPLICATE KEY UPDATE keyID = :keyID, vCode = :vCode, userID = :userID', array(':keyID' => $apiKey,
+            ':vCode' => $vCode,
+            ':userID' => $userID,
+        ));
+    }
 
-				/**
-				 * @param $apiKey
-				 *
-				 * @return array
-				 */
-				public function getAPIKey($apiKey)
-				{
-								return $this->db->queryRow('SELECT * FROM apiKeys WHERE keyID = :keyID', array(':keyID' => $apiKey));
-				}
+    /**
+     * @param $apiKey
+     *
+     * @return array
+     */
+    public function getAPIKey($apiKey)
+    {
+        return $this->db->queryRow('SELECT * FROM apiKeys WHERE keyID = :keyID', array(':keyID' => $apiKey));
+    }
 
-				/**
-					* @param $keyID
-					*
-					* @return null
-					*/
+    /**
+     * @param $keyID
+     *
+     * @return null
+     */
     public function getVCodeByKeyID($keyID)
-				{
-								return $this->db->queryField("SELECT vCode FROM apiKeys WHERE keyID = :keyID", "vCode", array(":keyID" => $keyID));
-				}
+    {
+        return $this->db->queryField("SELECT vCode FROM apiKeys WHERE keyID = :keyID", "vCode", array(":keyID" => $keyID));
+    }
 
-				/**
-				 * @param $apiKey
-				 *
-				 * @return boolean
-				 */
-				public function deleteAPIKey($apiKey)
-				{
-								return (bool) $this->db->execute('DELETE FROM apiKeys WHERE keyID = :keyID', array(':keyID' => $apiKey));
-				}
+    /**
+     * @param $apiKey
+     *
+     * @return boolean
+     */
+    public function deleteAPIKey($apiKey)
+    {
+        return (bool)$this->db->execute('DELETE FROM apiKeys WHERE keyID = :keyID', array(':keyID' => $apiKey));
+    }
 
-				/**
-					* Sets last validation to 5 minutes into the future pr. default
-					*
-					* @param $keyID
-					* @param int $lastValidated
-					*
-					* @return bool|int|string
-					*/
+    /**
+     * Sets last validation to 5 minutes into the future pr. default
+     *
+     * @param $keyID
+     * @param int $lastValidated
+     *
+     * @return bool|int|string
+     */
     public function updateLastValidated($keyID, $lastValidated = 0)
-				{
-								if(!$lastValidated)
-												$lastValidated = date("Y-m-d H:i:s", time() + 600);
-								if($lastValidated)
-												$lastValidated = date("Y-m-d H:i:s", time() + $lastValidated);
+    {
+        if (!$lastValidated)
+            $lastValidated = date("Y-m-d H:i:s", time() + 600);
+        if ($lastValidated)
+            $lastValidated = date("Y-m-d H:i:s", time() + $lastValidated);
 
-								return $this->db->execute("UPDATE apiKeys SET lastValidation = :lastValidation WHERE keyID = :keyID", array(":lastValidation" => $lastValidated, ":keyID" => $keyID));
-				}
+        return $this->db->execute("UPDATE apiKeys SET lastValidation = :lastValidation WHERE keyID = :keyID", array(":lastValidation" => $lastValidated, ":keyID" => $keyID));
+    }
 
-				/**
-					* @param $keyID
-					* @param $errorCode
-					*
-					* @return bool|int|string
-					*/
+    /**
+     * @param $keyID
+     * @param $errorCode
+     *
+     * @return bool|int|string
+     */
     public function setErrorCode($keyID, $errorCode)
-				{
-								return $this->db->execute("UPDATE apiKeys SET errorCode = :errorCode WHERE keyID = :keyID", array(":errorCode" => $errorCode, ":keyID" => $keyID));
-				}
+    {
+        return $this->db->execute("UPDATE apiKeys SET errorCode = :errorCode WHERE keyID = :keyID", array(":errorCode" => $errorCode, ":keyID" => $keyID));
+    }
 }
---
+++ src/Model/EVE/corporations.php
@@ -0,0 +1,262 @@
+<?php
+namespace ProjectRena\Model\EVE;
+
+use ProjectRena\RenaApp;
+
+/**
+ * Class invTypes
+ *
+ * @package ProjectRena\Model\EVE
+ */
+class corporations
+{
+    /**
+     * @var RenaApp
+     */
+    private $app;
+    /**
+     * @var \ProjectRena\Lib\Db
+     */
+    private $db;
+
+    /**
+     * @param RenaApp $app
+     */
+    function __construct(RenaApp $app)
+    {
+        $this->app = $app;
+        $this->db = $this->app->Db;
+    }
+
+    /**
+     * @param $corporationID
+     *
+     * @return array
+     */
+    public function getAllByID($corporationID)
+    {
+        return $this->db->queryRow("SELECT * FROM corporations WHERE corporationID = :id", array(":id" => $corporationID), 3600);
+    }
+
+    /**
+     * @param $corporationName
+     *
+     * @return array
+     */
+    public function getAllByName($corporationName)
+    {
+        return $this->db->queryRow("SELECT * FROM corporations WHERE corporationName = :name", array(":name" => $corporationName), 3600);
+    }
+
+    /**
+     * @param $allianceID
+     *
+     * @return array
+     */
+    public function getAllByAllianceID($allianceID)
+    {
+        return $this->db->queryRow("SELECT * FROM corporations WHERE allianceID = :id", array(":id" => $allianceID), 3600);
+    }
+
+    /**
+     * @param $corporationID
+     *
+     * @return null
+     */
+    public function getInformationByID($corporationID)
+    {
+        return $this->db->queryField("SELECT history FROM corporations WHERE corporationID = :id", "history", array(":id" => $corporationID), 3600);
+    }
+
+    /**
+     * @param $corporationName
+     *
+     * @return null
+     */
+    public function getInformationByName($corporationName)
+    {
+        return $this->db->queryField("SELECT history FROM corporations WHERE corporationName = :name", "history", array(":name" => $corporationName), 3600);
+    }
+
+    /**
+     * @param $corporationID
+     *
+     * @return null
+     */
+    public function getCEOIDByID($corporationID)
+    {
+        return $this->db->queryField("SELECT ceoID FROM corporations WHERE corporationID = :id", "ceoID", array(":id" => $corporationID), 3600);
+    }
+
+    /**
+     * @param $corporationName
+     *
+     * @return null
+     */
+    public function getCEOIDByName($corporationName)
+    {
+        return $this->db->queryField("SELECT ceoID FROM corporations WHERE corporationName = :name", "ceoID", array(":name" => $corporationName), 3600);
+    }
+
+    /**
+     * @param $corporationID
+     *
+     * @return null
+     */
+    public function getCorpTickerByID($corporationID)
+    {
+        return $this->db->queryField("SELECT corpTicker FROM corporations WHERE corporationID = :id", "corpTicker", array(":id" => $corporationID), 3600);
+    }
+
+    /**
+     * @param $corporationName
+     *
+     * @return null
+     */
+    public function getCorpTickerByName($corporationName)
+    {
+        return $this->db->queryField("SELECT corpTicker FROM corporations WHERE corporationName = :name", "corpTicker", array(":name" => $corporationName), 3600);
+    }
+
+    /**
+     * @param $corporationID
+     *
+     * @return null
+     */
+    public function getMemberCountByID($corporationID)
+    {
+        return $this->db->queryField("SELECT memberCount FROM corporations WHERE corporationID = :id", "memberCount", array(":id" => $corporationID), 3600);
+    }
+
+    /**
+     * @param $corporationName
+     *
+     * @return null
+     */
+    public function getMemberCountByName($corporationName)
+    {
+        return $this->db->queryField("SELECT memberCount FROM corporations WHERE corporationName = :name", "memberCount", array(":name" => $corporationName), 3600);
+    }
+
+    /**
+     * @param $corporationID
+     * @return array|bool
+     * @throws \Exception
+     */
+    public function getMembersByID($corporationID)
+    {
+        return $this->db->query("SELECT characterID, characterName FROM characters WHERE corporationID = :corporationID", array(":corporationID" => $corporationID), 3600);
+    }
+
+    /**
+     * @param $corporationName
+     * @return array|bool
+     * @throws \Exception
+     */
+    public function getMembersByName($corporationName)
+    {
+        return $this->db->query("SELECT characterID, characterName FROM characters WHERE corporationID = (SELECT corporationID FROM corporations WHERE corporationName = :corporationName)", array(":corporationName" => $corporationName), 3600);
+    }
+
+    /**
+     * @param $corporationID
+     * @param null $allianceID
+     * @param null $corporationName
+     * @param null $ceoID
+     * @param null $corpTicker
+     * @param null $memberCount
+     * @param null $information
+     */
+    public function updateCorporationDetails(
+     $corporationID,
+     $allianceID = null,
+     $corporationName = null,
+     $ceoID = null,
+     $corpTicker = null,
+     $memberCount = null,
+     $information = null
+    )
+    {
+        if(!empty($corporationID))
+        {
+            $exists = $this->getAllByID($corporationID);
+            if($exists)
+            {
+                if($allianceID) $this->db->execute("UPDATE corporations SET allianceID = :allianceID WHERE corporationID = :corporationID", array(
+                 "allianceID"   => $allianceID,
+                 ":corporationID" => $corporationID,
+                ));
+                if($corporationName) $this->db->execute("UPDATE corporations SET corporationName = :corporationName WHERE corporationID = :corporationID", array(
+                 "corporationName" => $corporationName,
+                 ":corporationID"  => $corporationID,
+                ));
+                if($ceoID) $this->db->execute("UPDATE corporations SET ceoID = :ceoID WHERE corporationID = :corporationID", array(
+                 ":ceoID" => $ceoID,
+                 ":corporationID"  => $corporationID,
+                ));
+                if($corpTicker) $this->db->execute("UPDATE corporations SET corpTicker = :corpTicker WHERE corporationID = :corporationID", array(
+                 ":corpTicker" => $corpTicker,
+                 ":corporationID"  => $corporationID,
+                ));
+                if($memberCount) $this->db->execute("UPDATE corporations SET memberCount = :memberCount WHERE corporationID = :corporationID", array(
+                 ":memberCount" => $memberCount,
+                 ":corporationID"  => $corporationID,
+                ));
+                if($information) $this->db->execute("UPDATE corporations SET information = :information WHERE corporationID = :corporationID", array(
+                 ":information"      => $information,
+                 ":corporationID" => $corporationID,
+                ));
+
+            } elseif(!empty($corporationID) && !empty($allianceID) && !empty($corporationName) && !empty($ceoID) && !empty($corpTicker) && !empty($memberCount) && !empty($information))
+            {
+                $this->db->execute("INSERT INTO corporations (corporationID, allianceID, corporationName, ceoID, corpTicker, memberCount, information) VALUES (:corporationID, :allianceID, :corporationName, :ceoID, :corpTicker, :memberCount, :information) ON DUPLICATE KEY UPDATE corporationID = :corporationID, allianceID = :allianceID, corporationName = :corporationName, ceoID = :ceoID, corpTicker = :corpTicker, memberCount = :memberCount, information = :information", array(
+                 ":corporationID" => $corporationID,
+                 ":allianceID"    => $allianceID,
+                 ":corporationName" => $corporationName,
+                 ":ceoID" => $ceoID,
+                 ":corpTicker" => $corpTicker,
+                 ":memberCount" => $memberCount,
+                 ":information" => $information,
+                ));
+            }
+        }
+    }
+
+    /**
+     * @param $corporationID
+     */
+    public function insertCorporationID($corporationID)
+    {
+        $this->db->execute("INSERT IGNORE INTO corporations (corporationID) VALUES (:corporationID)", array(":corporationID" => $corporationID));
+    }
+
+    /**
+     * @param $corporationID
+     * @param $lastUpdated
+     */
+    public function setLastUpdated($corporationID, $lastUpdated)
+    {
+        if($lastUpdated)
+            $this->db->execute("UPDATE corporations SET lastUpdated = :lastUpdated WHERE corporationID = :corporationID", array(":lastUpdated" => $lastUpdated, ":corporationID" => $corporationID));
+    }
+
+    /**
+     * @param $corporationID
+     * @param $ceoID
+     */
+    public function setCEOID($corporationID, $ceoID)
+    {
+        if($ceoID)
+            $this->db->execute("UPDATE corporations SET ceoID = :ceoID WHERE corporationID = :corporationID", array(":ceoID" => $ceoID, ":corporationID" => $corporationID));
+    }
+
+    /**
+     * @param $corporationID
+     * @param $memberCount
+     */
+    public function setMemberCount($corporationID, $memberCount)
+    {
+        if($memberCount)
+            $this->db->execute("UPDATE corporations SET memberCount = :memberCount WHERE corporationID = :corporationID", array(":memberCount" => $memberCount, ":corporationID" => $corporationID));
+    }
+}
