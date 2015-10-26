e336275ba4b00e906e396da5718b6b1f8b57929f
Michael Karbowiak <karbowiak@gmail.com>
2015-10-26 23:13:47 +0100

    Update composer.lock

--- src/Task/CCPDataTask.php
+++ src/Task/CCPDataTask.php
@@ -15,80 +15,75 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class CCPDataTask extends Command
 {
-				/**
-				 *
-				 */
-				protected function configure()
-				{
-								$this->setName('update:ccp')->setDescription('Updates the CCP database tables');
-				}
+    /**
+     *
+     */
+    protected function configure()
+    {
+        $this->setName('update:ccp')->setDescription('Updates the CCP database tables');
+    }
 
-				/**
-				 * @param InputInterface $input
-				 * @param OutputInterface $output
-				 *
-				 * @return int|null|void
-				 */
-				protected function execute(InputInterface $input, OutputInterface $output)
-				{
-								$app = RenaApp::getInstance();
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $app = RenaApp::getInstance();
 
-								// Setup the url and cache path
-								$url = "https://www.fuzzwork.co.uk/dump/";
-								$cache = __DIR__ . "/../../cache/update";
+        // Setup the url and cache path
+        $url = "https://www.fuzzwork.co.uk/dump/";
+        $cache = __DIR__ . "/../../cache/update";
 
-								// Create the cache dir if it doesn't exist
-								if(!file_exists($cache)) mkdir($cache);
+        // Create the cache dir if it doesn't exist
+        if (!file_exists($cache)) mkdir($cache);
 
-								// Fetch the md5
-								$md5file = "mysql-latest.tar.bz2.md5";
-								$md5 = explode(" ", $app->cURL->getData($url . $md5file, 0))[0];
+        // Fetch the md5
+        $md5file = "mysql-latest.tar.bz2.md5";
+        $md5 = explode(" ", $app->cURL->getData($url . $md5file, 0))[0];
 
-								$lastSeenMD5 = $app->Storage->get("ccpdataMD5");
+        $lastSeenMD5 = $app->Storage->get("ccpdataMD5");
 
-								if($lastSeenMD5 != $md5)
-								{
-												$output->writeln("Updating to latest CCP data dump");
-												$dbFiles = array(
-													"dgmAttributeCategories",
-													"dgmAttributeTypes",
-													"dgmEffects",
-													"dgmTypeAttributes",
-													"dgmTypeEffects",
-													"invFlags",
-													"invGroups",
-													"invTypes",
-													"mapDenormalize",
-													"mapRegions",
-													"mapSolarSystems",
-												);
-												$type = ".sql.bz2";
+        if ($lastSeenMD5 != $md5) {
+            $output->writeln("Updating to latest CCP data dump");
+            $dbFiles = array(
+                "dgmAttributeCategories",
+                "dgmAttributeTypes",
+                "dgmEffects",
+                "dgmTypeAttributes",
+                "dgmTypeEffects",
+                "invFlags",
+                "invGroups",
+                "invTypes",
+                "mapDenormalize",
+                "mapRegions",
+                "mapSolarSystems",
+            );
+            $type = ".sql.bz2";
 
-												foreach($dbFiles as $file)
-												{
-																$dataURL = $url . "latest/" . $file . $type;
-																try
-																{
-																				exec("wget -q {$dataURL} -O $cache/$file$type");
-																				exec("bzip2 -q -d $cache/$file$type");
-																} catch(\Exception $e)
-																{
-																				echo "Error";
-																}
+            foreach ($dbFiles as $file) {
+                $dataURL = $url . "latest/" . $file . $type;
+                try {
+                    exec("wget -q {$dataURL} -O $cache/$file$type");
+                    exec("bzip2 -q -d $cache/$file$type");
+                } catch (\Exception $e) {
+                    echo "Error";
+                }
 
-																$data = file_get_contents($cache . "/" . $file . ".sql");
-																$data = str_replace("ENGINE=InnoDB", "ENGINE=TokuDB", $data);
-																$dataParts = explode(";\n", $data);
-																foreach($dataParts as $qry)
-																{
-																				$query = $qry . ";";
-																				$app->Db->execute($query);
-																}
+                $data = file_get_contents($cache . "/" . $file . ".sql");
+                $data = str_replace("ENGINE=InnoDB", "ENGINE=TokuDB", $data);
+                $dataParts = explode(";\n", $data);
+                foreach ($dataParts as $qry) {
+                    $query = $qry . ";";
+                    $app->Db->execute($query);
+                }
 
-																unlink("{$cache}/{$file}.sql");
-												}
+                unlink("{$cache}/{$file}.sql");
+            }
 
-												$app->Storage->set("ccpdataMD5", $md5);
-								}
-				}
+            $app->Storage->set("ccpdataMD5", $md5);
+        }
+    }
 }
